10987d846fa76638f68021f2d3871ade2ce26ebe
JrFellow <160649111+JrFellow@users.noreply.github.com>
2024-07-06 13:47:58 -0700

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,6 @@
+# auth information
+GDGF = ['N', 'N', '6364', 'gjyjgj', 0]
+GEGE = ['N', 'N', '6565', 'n', 19326]
+AJCD = ['Liam', 'Haider', '0913', 'random', 26558]
+GHGH = ['N', 'N', '6767', 'gdgdgdg', 0]
+AJCD = ['Liam', 'Haider', '0913', 'liam@123', 0]
