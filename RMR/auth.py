8022299a1caf5a26cf2023a206101dcd1b0e2476
JrFellow <160649111+JrFellow@users.noreply.github.com>
2024-07-06 13:50:19 -0700

    Delete RMR directory

--- RMR/auth.py
+++
@@ -1,5 +0,0 @@
-# auth information
-GDGF = ['N', 'N', '6364', 'gjyjgj', 0]
-GEGE = ['N', 'N', '6565', 'n', 24058]
-AJCD = ['Liam', 'Haider', '0913', 'random', 0]
-GHGH = ['N', 'N', '6767', 'gdgdgdg', 0]
